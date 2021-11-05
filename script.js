6460986cb9aa1edd3640b09e5538d5a93f0d0af5
devAltss <altoss11@gmail.com>
2021-11-05 17:05:24 +0200

    function training

--- script.js
+++ script.js
@@ -81,23 +81,65 @@
 // }
 
 
-let num = 50;
+//let num = 50;
 // while (num < 55) {
 //   console.log(num);
 //   num++;
 // }
 
-do {
-  console.log(num);
-  num++;
-} while (num < 55)
+// do {
+//   console.log(num);
+//   num++;
+// } while (num < 55)
 
 
-for (let i = 1; i < 8; i++) {
-  if (i == 6) {
-    continue;
-  }
-  console.log(i);
+// for (let i = 1; i < 8; i++) {
+//   if (i == 6) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//   alert(text);
+//   //let num = 10;
+//   console.log(num);
+// }
+
+// showFirstMessage("Hello World!");
+// console.log(num);
+
+
+// let calc = function(a, b) {
+//   return (a + b);
+// }
+
+
+let calc = (a, b) => a + b
+
+console.log(calc(3, 4));
+
+console.log(calc(8, 4));
+
+
+function retVar() {
+  let num = 50;
+  return num;
 }
 
+let anotherNum = retVar();
+console.log(anotherNum);
 
+let str = "Test";
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let twelve = "12.2px";
+//console.log(Math.round(twelve));
+console.log(parseInt(twelve));
+console.log(parseFloat(twelve));
